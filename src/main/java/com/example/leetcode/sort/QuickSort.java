92b2dda253169256df9ae13938a0e6bead431280
qinyudong <qinyudong89@gemail.com>
2019-05-26 22:04:53 +0800

    快速排序优化

--- src/main/java/com/example/leetcode/sort/QuickSort.java
+++ src/main/java/com/example/leetcode/sort/QuickSort.java
@@ -1,59 +1,53 @@
 package com.example.leetcode.sort;
 
-import java.util.Arrays;
-
 /**
  * @author yu dong qin
  * @ClassName: QuickSort
  * @Description: 快速排序
- * @date 2019/5/25 17:31
+ * @date 2019/5/25 22:35
  */
 public class QuickSort {
-    
-    public static int[] quickSort(int[] array, int low, int high){
-        if (low > high){
+    /**
+     * @return int[]
+     * @Description
+     * @Param [array, low, high]
+     **/
+    public static int[] quickSort(int[] array, int low, int high) {
+        if (low > high) {
             return array;
         }
-        /** 作用在于找到大于基准数 */
+        int pivot = partition(array, low, high);
+        quickSort(array, low, pivot - 1);
+        quickSort(array, pivot + 1, high);
+        return array;
+    }
+
+    /**
+     * @return int
+     * @Description 分区
+     * @Param [array, low, high]
+     **/
+    private static int partition(int[] array, int low, int high) {
         int left = low;
-        /** 作用在于找到小于基准数 */
         int right = high;
-        /** 基准元素 */
         int base = array[low];
-
-        /**
-         * 1、分区（小于基准数的放在左边，大于基准数的放在右边）
-         * 注：当left == right时表明找到基准元素的位置
-         * */
-        while (left < right){
-            /** 从右向左遍历，直到找到小于基准元素*/
-            while ( array[right] >= base && left < right){
-                right--;
-            }
-
-            /** 从左向右遍历，直到找到大于基准元素 */
-            while (array[left] <= base && left < right){
+        //while这里的作用是将小于、大于基准点的元素，分别划分到基准点的两侧
+        while (left < right) {
+            while (array[left] <= base) {
                 left++;
             }
-            if (left < right){
+            while (array[right] > base) {
+                right--;
+            }
+            if (left < right) {
                 int temp = array[right];
                 array[right] = array[left];
                 array[left] = temp;
             }
         }
-        /** 2、将基准元素归位(此时left == right 说明已经找到基准元素的位置) */
-        array[low] = array[left];
-        array[left] = base;
-
-        /** 遍历排序右边数组 */
-        quickSort(array, low, right - 1);
-        /** 遍历排序左边数组 */
-        quickSort(array, right + 1, high);
-        return array;
-    }
-
-    public static void main(String[] args) {
-        int[] array = {6, 1, 2, 7, 9, 3, 4, 5, 10, 8};
-        System.out.println(Arrays.toString(quickSort(array, 0, array.length -1)));
+        //将基准点归位（left == rigth时表明，基准点找到）
+        array[low] = array[right];
+        array[right] = base;
+        return right;
     }
 }
