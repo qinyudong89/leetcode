b4d240b052bf9821a9ccd59bfe2be83bc3016c08
qinyudong <qinyudong89@gemail.com>
2019-05-26 22:01:41 +0800

    插入排序

--- src/main/java/com/example/leetcode/sort/QuickSort2.java
+++ src/main/java/com/example/leetcode/sort/QuickSort2.java
@@ -1,7 +1,5 @@
 package com.example.leetcode.sort;
 
-import java.util.Arrays;
-
 /**
  * @author yu dong qin
  * @ClassName: QuickSort2
@@ -10,40 +8,38 @@ import java.util.Arrays;
  */
 public class QuickSort2 {
     /**
-     *
-     * @Description  
-     * @Param [array, low, high]
      * @return int[]
+     * @Description
+     * @Param [array, low, high]
      **/
-    public static int[] quickSort(int[] array, int low, int high){
-        if (low > high){
+    public static int[] quickSort(int[] array, int low, int high) {
+        if (low > high) {
             return array;
         }
         int pivot = partition(array, low, high);
-        quickSort(array, low, pivot -1);
+        quickSort(array, low, pivot - 1);
         quickSort(array, pivot + 1, high);
         return array;
     }
-    
+
     /**
-     *
-     * @Description  分区
-     * @Param [array, low, high]
      * @return int
+     * @Description 分区
+     * @Param [array, low, high]
      **/
     private static int partition(int[] array, int low, int high) {
         int left = low;
         int right = high;
         int base = array[low];
         //while这里的作用是将小于、大于基准点的元素，分别划分到基准点的两侧
-        while (left < right){
-            while (array[left] <= base){
+        while (left < right) {
+            while (array[left] <= base) {
                 left++;
             }
-            while (array[right] > base){
+            while (array[right] > base) {
                 right--;
             }
-            if (left < right){
+            if (left < right) {
                 int temp = array[right];
                 array[right] = array[left];
                 array[left] = temp;
@@ -53,10 +49,5 @@ public class QuickSort2 {
         array[low] = array[right];
         array[right] = base;
         return right;
-    }
-
-    public static void main(String[] args) {
-        int[] array = {6, 1, 2, 7, 9, 3, 4, 5, 10, 8};
-        System.out.println(Arrays.toString(quickSort(array, 0, array.length -1)));
     }
 }
