3cc324d350fce12640bc41502923a75fa2c84067
qinyudong <yudong.qin@topideal.com.cn>
2019-05-28 15:09:55 +0800

    33. 搜索旋转排序数组

---
+++ src/main/java/com/example/leetcode/bsearch/ScrewArray.java
@@ -0,0 +1,45 @@
+package com.example.leetcode.bsearch;
+
+/**
+ * @Description:33. 搜索旋转排序数组
+ * @Auther: 覃钰栋
+ * @Date: 2019/5/28 15:02
+ */
+public class ScrewArray {
+
+    //4，5，6，1，2，3  5
+    public static int search(int[] array, int target) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (array[mid] == target) {
+                return mid;
+            }
+
+            //情况一、如果首元素小于 mid，说明前半部分是有序的，后半部分是循环有序数组
+            if (array[low] <= array[mid]) {
+                if (target < array[mid] && target >= array[low]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            //情况二、如果尾元素大于 mid，说明后半部分是有序的，前半部分是循环有序的数组；
+            if (array[high] >= array[mid]) {
+                if (target > array[mid] && target <= array[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4,5,6,1,2,3};
+        search(array, 5);
+    }
+}
