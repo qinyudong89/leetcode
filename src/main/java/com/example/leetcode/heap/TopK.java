ca6826d39b13a0838c530bd4e819a0500eb9caae
qinyudong <qinyudong89@gemail.com>
2019-06-08 14:49:37 +0800

    求topK优化

--- src/main/java/com/example/leetcode/heap/TopK.java
+++ src/main/java/com/example/leetcode/heap/TopK.java
@@ -11,24 +11,26 @@ import java.util.PriorityQueue;
 public class TopK {
 
     public static int[] topk(int[] array, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>(k);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
 
-        //1、构建k大小的堆
+        //1、构建k大小的小顶堆
         for (int i : array) {
-            if (queue.size() < k) {
-                queue.offer(i);
+            if (minHeap.size() < k) {
+                minHeap.offer(i);
             } else {
                 //比堆顶大的，则存入
-                if (i > queue.peek()) {
-                    queue.poll();
-                    queue.offer(i);
+                if (i > minHeap.peek()) {
+                    minHeap.poll();
+                    minHeap.offer(i);
                 }
             }
         }
-        //2、queue就是前K的大数据
+        //2、minHeap就是前K的大数据
         int[] result = new int[k];
-        for (int j = 0; j < k && !queue.isEmpty(); j++) {
-            result[j] = queue.poll();
+        int i = 0;
+        while (!minHeap.isEmpty()) {
+            result[i] = minHeap.poll();
+            i++;
         }
         return result;
     }
