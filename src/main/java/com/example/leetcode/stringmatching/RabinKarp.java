65211a8190b98eda75bccbfd196e0f19d552347f
qinyudong <yudong.qin@topideal.com.cn>
2019-06-12 14:40:27 +0800

    Rabin-Karp算法

--- src/main/java/com/example/leetcode/stringmatching/RabinKarp.java
+++ src/main/java/com/example/leetcode/stringmatching/RabinKarp.java
@@ -9,7 +9,7 @@ import java.util.Random;
  * @Date: 2019/6/11 17:33
  */
 public class RabinKarp {
-    //质数
+    //质数(减少哈希冲突)
     private long prime;
     //进制
     private int denary = 256;
@@ -18,20 +18,20 @@ public class RabinKarp {
     //模式串长度
     private int m;
     //模式hash
-    private long patternHash;
-    //text的第一个子串的hash值（用以后续计算）
-    private long baseHash;
+    private long p;
+    //
+    private long h;
 
     //初始化
     public void init(String text, String pattern) {
         this.n = text.length();
         this.m = pattern.length();
         this.prime = randomPrime();
-        baseHash = 1;
+        h = 1;
         for (int i = 1; i <= m - 1; i++) {
-            baseHash = (denary * baseHash) % prime;
+            h = (denary * h) % prime;
         }
-        patternHash = hash(pattern, m);
+        p = hash(pattern, m);
     }
 
     //搜索
@@ -41,18 +41,18 @@ public class RabinKarp {
             return -1;
         }
         long textHash = hash(text, m);
-        if ((patternHash == textHash) && check(text, pattern, 0)) {
+        if ((p == textHash) && check(text, pattern, 0)) {
             return 0;
         }
 
         for (int i = m; i < n; i++) {
-            //利用上一个子串的hash结果hash(Si-m+1)，在O(1)的时间内计算出hash(Si-m+2)
-            textHash = (textHash + prime - baseHash * text.charAt(i - m) % prime) % prime;
+            //利用上一个子串的hash，在O(1)的时间内计算出下一个子串的hash
+            textHash = (textHash + prime - h * text.charAt(i - m) % prime) % prime;
             textHash = (textHash * denary + text.charAt(i)) % prime;
             //计算下标
             int offset = i - m + 1;
-            //使用“拉斯维加斯方法则”，在比较散列值相同后，再去比较字符，保证在散列冲突的情况下，仍能保证正确性
-            if ((patternHash == textHash) && check(text, pattern, offset)) {
+            //在比较散列值相同后，使用“拉斯维加斯方法则”，再去比较字符，保证在散列冲突的情况下，仍能保证正确性
+            if ((p == textHash) && check(text, pattern, offset)) {
                 return offset;
             }
         }
@@ -71,12 +71,12 @@ public class RabinKarp {
 
     //计算hash
     private long hash(String key, int m) {
-        long h = 0;
+        long hash = 0;
         for (int j = 0; j < m; j++) {
             //使用霍纳法则，快速解决“多项式求值”
-            h = (denary * h + key.charAt(j)) % prime;
+            hash = (denary * hash + key.charAt(j)) % prime;
         }
-        return h;
+        return hash;
     }
 
     //随机生成素数
@@ -87,7 +87,7 @@ public class RabinKarp {
 
     public static void main(String[] args) {
         RabinKarp rabinKarp = new RabinKarp();
-        int index = rabinKarp.search("errtyyye", "ye");
+        int index = rabinKarp.search("面试算法之字符串匹配算法,Rabin-Karp算法详解 09-08 阅读数 3709 既然谈论到字符串相关算法,那么字符串匹配是根本绕不过去的坎。在面试中,面试官可", "既然谈论到字");
         System.out.println(index);
     }
 }
