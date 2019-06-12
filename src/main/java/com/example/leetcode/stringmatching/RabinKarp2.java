5e0b3ac1e3c0f1bc69751dea6072daa6c20f1a46
qinyudong <yudong.qin@topideal.com.cn>
2019-06-12 17:47:32 +0800

    Rabin-Karp算法

--- src/main/java/com/example/leetcode/stringmatching/RabinKarp2.java
+++ src/main/java/com/example/leetcode/stringmatching/RabinKarp2.java
@@ -1,8 +1,5 @@
 package com.example.leetcode.stringmatching;
 
-import java.math.BigInteger;
-import java.util.Random;
-
 /**
  * @Description:Rabin-Karp算法
  * @Auther: 覃钰栋
@@ -11,61 +8,46 @@ import java.util.Random;
 public class RabinKarp2 {
     // d是进制
     public final static int d = 256;
+    //质数
+    public final static int q = 9001;
 
-    static void search(String text, String pattern) {
-        int M = pattern.length();
-        int N = text.length();
-        int i, j;
-        //模式哈希值
+    public static int search(String text, String pattern) {
+        int n = text.length();
+        int m = pattern.length();
         int p = 0;
-        //文本哈希值
         int t = 0;
         int h = 1;
-        //质数
-        int q = randomPrime();
-
-        // h的值为“pow(d, M-1)%q”
-        for (i = 0; i < M - 1; i++) {
-            h = (h * d) % q;
+        //1、h的值为“pow(d, M-1)%q”
+        for (int i = 0; i < m - 1; i++) {
+            h = (d * h) % q;
         }
 
-        //首先计算模式的哈希值、及窗口的文本
-        for (i = 0; i < M; i++) {
-            p = (d * p + pattern.charAt(i)) % q;
+        //2、计算pattern的哈希值，及text第一个子串的哈希值
+        for (int i = 0; i < m; i++) {
             t = (d * t + text.charAt(i)) % q;
+            p = (d * p + pattern.charAt(i)) % q;
         }
-
-        //将模式与文本的每个窗口比较
-        for (i = 0; i <= N - M; i++) {
-            //如果模式哈希值与文本窗口的哈希值相等，则比较字符串
-            if (p == t) {
-                for (j = 0; j < M; j++) {
-                    if (text.charAt(i + j) != pattern.charAt(j)) {
-                        break;
-                    }
+        //3、将text中的每个长度为m的子串的哈希值，与pattern的哈希值比较
+        for (int i = 0; i <= n - m; i++) {
+            if (t == p) {
+                int j = 0;
+                while (j < m && (pattern.charAt(j) == text.charAt(i + j))) {
+                    j++;
                 }
-                // if p == t and pat[0...M-1] = txt[i, i+1, ...i+M-1]
-                if (j == M) {
-                    System.out.println("Pattern found at index " + i);
+                //j==m则说明已经找到匹配的字符串
+                if (j == m) {
+                    return i;
                 }
             }
 
-            // 计算下一个文本窗口的哈希值: 删除前位数，添加后位数
-            if (i < N - M) {
-                t = (d * (t - text.charAt(i) * h) + text.charAt(i + M)) % q;
-
-                // 我们可能得到负的t，把它变成正的
+            //4、计算下一个子串的哈希值
+            if (i < n - m) {
+                t = (d * (t - text.charAt(i) * h) + text.charAt(i + m)) % q;
                 if (t < 0) {
-                    t = (t + q);
+                    t += q;
                 }
             }
         }
+        return -1;
     }
-
-    //随机生成素数
-    private static int randomPrime() {
-        BigInteger prime = BigInteger.probablePrime(31, new Random());
-        return prime.intValue();
-    }
-
 }
