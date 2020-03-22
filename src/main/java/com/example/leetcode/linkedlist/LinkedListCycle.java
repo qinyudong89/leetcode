f998f7a5a5c4f5dc76276a075f30695729b2872c
qinyudong89 <qinyudong89@126.com>
2020-03-22 21:28:53 +0800

    链表中环检测,增加新解法

--- src/main/java/com/example/leetcode/linkedlist/LinkedListCycle.java
+++ src/main/java/com/example/leetcode/linkedlist/LinkedListCycle.java
@@ -1,5 +1,8 @@
 package com.example.leetcode.linkedlist;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author yu dong qin
  * @ClassName: 141.环形链表（链表中环检测）
@@ -7,18 +10,49 @@ package com.example.leetcode.linkedlist;
  * @date
  */
 public class LinkedListCycle {
-    public boolean hasCycle(ListNode head) {
+
+    /**
+     * 解法一：通过Map
+     * 1、在遍历ListNode时，将val作为key
+     * 2、如果Key重复，则说明存在环
+     * 时间复杂度：O(n)
+     * 窝里复杂度：O(n)
+     * @param head
+     * @return
+     */
+    public boolean hasCycle1(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            } else {
+                set.add(head);
+                head = head.next;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解法二：通过快慢指针
+     * 时间复杂度：O(n)
+     * 窝里复杂度：O(1)
+     * @param head
+     * @return
+     */
+    public boolean hasCycle2(ListNode head) {
         if (head == null) {
             return false;
         }
-        //慢指针
+        ListNode fast = head;
         ListNode slow = head;
-        //快指针
-        ListNode fast = head.next;
-        while (slow != fast && fast != null && fast.next != null) {
+        while(fast.next != null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
         }
-        return fast == null ? false : fast == slow;
+        return false;
     }
 }
